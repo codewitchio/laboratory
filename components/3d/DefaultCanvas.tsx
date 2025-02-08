8af7768f5064895409183e5cbb1223ae152966b4
Hanna <hanna@codewitch.io>
2025-02-09 00:00:59 +0100

    Add Suspense to canvas

--- components/3d/DefaultCanvas.tsx
+++ components/3d/DefaultCanvas.tsx
@@ -1,5 +1,7 @@
+import LoadingIndicator from "@/components/LoadingIndicator"
 import { PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import { Suspense } from "react"
 
 export default function DefaultCanvas({
   children,
@@ -7,11 +9,15 @@ export default function DefaultCanvas({
   children: React.ReactNode
 }) {
   return (
-    <Canvas className="!absolute !inset-0">
-      <PerspectiveCamera makeDefault position={[0, 0, 5]} />
-      <ambientLight intensity={0.1} />
-      <directionalLight position={[5, 5, 5]} intensity={2} />
-      {children}
-    </Canvas>
+    <Suspense fallback={<LoadingIndicator />}>
+      <Canvas className="!absolute !inset-0">
+        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+        <ambientLight intensity={0.1} />
+        <directionalLight position={[5, 5, 5]} intensity={2} />
+        {children}
+        {/* Add useControls toggle for this? */}
+        {/* <Stats /> */}
+      </Canvas>
+    </Suspense>
   )
 }
