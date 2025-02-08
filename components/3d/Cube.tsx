78f89ca1b29d6aa544a310b2e1c2a4439b07ff6f
Hanna <hanna@codewitch.io>
2025-02-08 22:07:29 +0100

    Fix Cube resetting its material on re-render

--- components/3d/Cube.tsx
+++ components/3d/Cube.tsx
@@ -3,19 +3,11 @@
 import { useFloatAnimation } from "@/lib/three/animations"
 import { shaderMaterial } from "@react-three/drei"
 import { extend, useFrame } from "@react-three/fiber"
-import { useRef, useState } from "react"
+import { useMemo, useRef } from "react"
 import { Color, Mesh } from "three"
 
 export function Cube() {
   const mesh = useRef<Mesh>(null)
-  const [clicked, setClicked] = useState(false)
-
-  // const springs = useSpring({
-  //   color: clicked ? "#664BAF" : "#8E704E",
-  //   config: {
-  //     duration: 500,
-  //   },
-  // })
 
   useFloatAnimation({
     object: mesh,
@@ -26,25 +18,32 @@ export function Cube() {
     },
   })
 
-  const ColorShiftMaterial = shaderMaterial(
-    { time: 0, color: new Color(0.2, 0.0, 0.1) },
-    // vertex shader
-    /*glsl*/ `
-    varying vec2 vUv;
-    void main() {
-      vUv = uv;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-  `,
-    // fragment shader
-    /*glsl*/ `
-    uniform float time;
-    uniform vec3 color;
-    varying vec2 vUv;
-    void main() {
-      gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
-    }
-  `
+  const ColorShiftMaterial = useMemo(
+    () =>
+      shaderMaterial(
+        {
+          time: 0,
+          color: new Color(0.2, 0.0, 0.1),
+        },
+        // vertex shader
+        /*glsl*/ `
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `,
+        // fragment shader
+        /*glsl*/ `
+        uniform float time;
+        uniform vec3 color;
+        varying vec2 vUv;
+        void main() {
+          gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
+        }
+      `
+      ),
+    []
   )
 
   extend({ ColorShiftMaterial })
@@ -56,16 +55,12 @@ export function Cube() {
   })
 
   return (
-    <mesh
-      ref={mesh}
-      position={[0, 0, 0]}
-      rotation={[0, 0, 0]}
-      onClick={() => setClicked(!clicked)}
-    >
+    <mesh ref={mesh} position={[0, 0, 0]} rotation={[0, 0, 0]}>
       <boxGeometry />
       {/* TODO: Look into adding the type to ThreeElements, after moving the material to its own file: https://github.com/pmndrs/leva */}
       {/* @ts-expect-error React doesn't know we used extend */}
       <colorShiftMaterial
+        // TODO: Set colour from leva/useControls
         color="#664BAF"
         time={1}
         ref={colorShiftMaterial}
