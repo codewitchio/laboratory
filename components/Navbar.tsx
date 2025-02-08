a6d9015cddfa1171f350ee8fe98964ba68fea87d
Hanna <hanna@codewitch.io>
2025-02-08 19:13:47 +0100

    Add layout animations to NavBar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,45 @@
+"use client"
+import { AnimatePresence, motion } from "motion/react"
 import Image from "next/image"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+const AnimatedLink = motion.create(Link)
 
 export function Navbar() {
+  const pathname = usePathname()
+  const isHomePage = pathname === "/"
+
   return (
-    <nav className="card relative z-50 m-auto my-4 flex flex-row bg-base-100 p-2 px-6">
-      {/* TODO: The antialiasing on the hover scaling doesn't look amazing */}
-      {/* TODO: Only show back button when on non-home pages */}
-      <Link
-        href="/"
-        className="me-2 flex cursor-pointer transition-all hover:scale-110"
-      >
-        <Image
-          src="icons/arrow-small-left.svg"
-          alt="Back"
-          width={24}
-          height={24}
-        />
-      </Link>
-      <Link href="/" className="text-xl font-light normal-case link-hover">
-        codewitch's laboratory
-      </Link>
-      {/* TODO: Add subtitle based on page/route */}
-    </nav>
+    <motion.nav
+      layout
+      className="card relative z-50 m-auto my-4 flex flex-row bg-base-100 p-2 px-6"
+    >
+      <AnimatePresence initial={false}>
+        {/* TODO: Add hover animation */}
+        {!isHomePage && (
+          <AnimatedLink
+            href="/"
+            className="flex cursor-pointer"
+            // layout animation doesn't seem to work with "gap", so we set a manual margin here instead
+            initial={{ opacity: 0, width: 0, marginRight: 0 }}
+            animate={{ opacity: 1, width: "auto", marginRight: 8 }}
+            exit={{ opacity: 0, width: 0, marginRight: 0 }}
+            key="back-button"
+          >
+            <Image
+              src="icons/arrow-small-left.svg"
+              alt="Back"
+              width={24}
+              height={24}
+            />
+          </AnimatedLink>
+        )}
+        <Link href="/" className="text-xl font-light normal-case link-hover">
+          codewitch's laboratory
+        </Link>
+        {/* TODO: Add subtitle based on page/route */}
+      </AnimatePresence>
+    </motion.nav>
   )
 }
