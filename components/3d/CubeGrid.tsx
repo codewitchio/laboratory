ab3364470351ed4003f99d3108e7607138405c25
Hanna <hanna@codewitch.io>
2025-02-10 20:52:37 +0100

    Refactor CubeGrid to use instancing and no react-spring

--- components/3d/CubeGrid.tsx
+++ components/3d/CubeGrid.tsx
@@ -1,11 +1,21 @@
 "use client"
 
-import { animated, useSprings } from "@react-spring/three"
 import { useThree } from "@react-three/fiber"
-import { useCallback, useEffect, useRef } from "react"
-import { Color, Plane, Raycaster, Vector2, Vector3 } from "three"
+import { MutableRefObject, useCallback, useEffect, useRef } from "react"
+import {
+  Color,
+  InstancedMesh,
+  Object3D,
+  Plane,
+  Raycaster,
+  Vector2,
+  Vector3,
+} from "three"
 
-const GRID_SIZE = 10
+const GRID_SIZE = 100
+const count = GRID_SIZE * GRID_SIZE
+const defaultColor = new Color("#439400")
+
 const indexToXY = (index: number) => {
   const i = Math.floor(index / GRID_SIZE)
   const j = index % GRID_SIZE
@@ -15,18 +25,13 @@ const indexToXY = (index: number) => {
 // For use in handlePointerMove
 const mouseVec = new Vector2()
 const intersection = new Vector3()
-const cubePos = new Vector3()
 const cubeColor = new Color()
+const temp = new Object3D()
 
 export function CubeGrid() {
   const isOver = useRef(false)
   const { size, camera } = useThree((state) => state)
-
-  // Single spring system for all cubes
-  const [springs, api] = useSprings(GRID_SIZE * GRID_SIZE, () => ({
-    scale: 1,
-    color: "#00ff00", // Initial green color
-  }))
+  const instancedMeshRef = useRef<InstancedMesh>(null!)
 
   const raycaster = useRef(new Raycaster())
   const plane = useRef(new Plane(new Vector3(0, 0, 1), 3)) // Correct constant for z = -5
@@ -37,11 +42,8 @@ export function CubeGrid() {
 
   const handlePointerLeave = useCallback(() => {
     isOver.current = false
-    api.start({
-      scale: 1,
-      color: "#00ff00", // Reset color to green
-    })
-  }, [api])
+    setCubesToDefault(instancedMeshRef)
+  }, [])
 
   const handlePointerMove = useCallback(
     (e: MouseEvent) => {
@@ -56,29 +58,28 @@ export function CubeGrid() {
       raycaster.current.setFromCamera(mouseVec, camera)
       raycaster.current.ray.intersectPlane(plane.current, intersection)
 
-      api.start((index) => {
-        const { x, y } = indexToXY(index)
-        // I'm sure we won't have to do this if we have a proper instantiation setup
-        cubePos.set(
-          x - GRID_SIZE / 2,
-          y - GRID_SIZE / 2,
-          -5 // Local position (group is at z: -5)
-        )
-
-        // Calculate true 3D distance
-        const distance = cubePos.distanceTo(intersection)
+      for (let i = 0; i < count; i++) {
+        instancedMeshRef.current.getMatrixAt(i, temp.matrix)
+        const distance = temp.position.distanceTo(intersection)
         const normalizedDistance = Math.min(distance / 12, 1) // Adjust divisor for sensitivity
 
         const hue = 120 * normalizedDistance // 120º (green)
-        const color = cubeColor.setHSL(hue / 360, 1, 0.5).getHexString()
+        cubeColor.setHSL(hue / 360, 1, 0.5)
 
-        return {
-          scale: Math.max(1, Math.min(1.25, 1.5 - normalizedDistance)),
-          color: `#${color}`,
-        }
-      })
+        const scale = Math.max(1, Math.min(1.25, 1.5 - normalizedDistance))
+        temp.scale.set(scale, scale, scale)
+
+        const { x, y } = indexToXY(i)
+        temp.position.set(x - GRID_SIZE / 2, y - GRID_SIZE / 2, 0)
+        temp.updateMatrix()
+
+        instancedMeshRef.current.setMatrixAt(i, temp.matrix)
+        instancedMeshRef.current.setColorAt(i, cubeColor)
+      }
+      instancedMeshRef.current.instanceMatrix.needsUpdate = true
+      instancedMeshRef.current.instanceColor!.needsUpdate = true
     },
-    [api, size, camera]
+    [size, camera]
   )
 
   useEffect(() => {
@@ -99,22 +100,35 @@ export function CubeGrid() {
     }
   }, [handlePointerEnter, handlePointerLeave, handlePointerMove])
 
+  useEffect(() => {
+    // Set initial positions
+    setCubesToDefault(instancedMeshRef)
+  }, [])
+
   return (
-    <group rotation={[-0.5, 0, 0]} position={[0, 0, -5]}>
-      {/* Existing cubes */}
-      {Array.from({ length: GRID_SIZE * GRID_SIZE }).map((_, index) => {
-        const { x, y } = indexToXY(index)
-        return (
-          <animated.mesh
-            key={`${x}-${y}`}
-            position={[x - GRID_SIZE / 2, y - GRID_SIZE / 2, 0]}
-            scale={springs[index].scale}
-          >
-            <boxGeometry args={[0.8, 0.8, 0.8]} />
-            <animated.meshStandardMaterial color={springs[index].color} />
-          </animated.mesh>
-        )
-      })}
-    </group>
+    <>
+      {/* <planeHelper args={[plane.current, 100, 0x33333]} /> */}
+      <group rotation={[-0.5, 0, 0]} position={[0, 0, -15]}>
+        <instancedMesh
+          ref={instancedMeshRef}
+          args={[undefined, undefined, count]}
+        >
+          <boxGeometry args={[0.8, 0.8, 0.8]} />
+          <meshStandardMaterial />
+        </instancedMesh>
+      </group>
+    </>
   )
+}
+
+function setCubesToDefault(instancedMeshRef: MutableRefObject<InstancedMesh>) {
+  for (let i = 0; i < count; i++) {
+    const { x, y } = indexToXY(i)
+    temp.position.set(x - GRID_SIZE / 2, y - GRID_SIZE / 2, 0)
+    temp.updateMatrix()
+    instancedMeshRef.current.setMatrixAt(i, temp.matrix)
+    instancedMeshRef.current.setColorAt(i, defaultColor)
+  }
+  instancedMeshRef.current.instanceMatrix.needsUpdate = true
+  instancedMeshRef.current.instanceColor!.needsUpdate = true
 }
