1d3342523aab4b0e3ed560d63b082ce770611874
Hanna <hanna@codewitch.io>
2025-03-25 18:58:43 +0100

    Overall improvements to DefaultCanvas

--- components/3d/DefaultCanvas.tsx
+++ components/3d/DefaultCanvas.tsx
@@ -1,36 +1,46 @@
 import LoadingIndicator from "@/components/LoadingIndicator"
 import { Bounds, OrbitControls, PerspectiveCamera } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { Canvas, CanvasProps } from "@react-three/fiber"
 import { Suspense } from "react"
 
 type control = "orbit" | "none"
 
 export default function DefaultCanvas({
   cameraPosition = [0, 0, 5],
+  lightPosition = [5, 5, 5],
   bounds = true,
   control = "orbit",
-  children,
+  ...props
 }: {
   cameraPosition?: [number, number, number]
+  lightPosition?: [number, number, number]
   bounds?: boolean
   control?: control
-  children: React.ReactNode
-}) {
+} & CanvasProps) {
   return (
     <Suspense fallback={<LoadingIndicator />}>
-      <Canvas className="!absolute !inset-0">
+      <Canvas className="!absolute !inset-0" shadows="soft" {...props}>
         <PerspectiveCamera makeDefault position={cameraPosition} />
-        <ambientLight intensity={0.1} />
-        <directionalLight position={[5, 5, 5]} intensity={2} />
+        {/* Ambient light for overall scene brightness */}
+        <ambientLight intensity={0.5} />
+        {/* Main directional light */}
+        <directionalLight
+          position={lightPosition}
+          intensity={1}
+          castShadow
+          // shadow-mapSize={[1024, 1024]} // Not sure if this does anything currently?
+        />
+        {/* Soft fill light */}
+        <directionalLight position={[-5, 3, -5]} intensity={0.3} />
 
         {control === "orbit" && <OrbitControls />}
 
         {bounds ? (
           <Bounds fit clip observe margin={1.5}>
-            {children}
+            {props.children}
           </Bounds>
         ) : (
-          children
+          props.children
         )}
 
         {/* Add useControls toggle for this? */}
